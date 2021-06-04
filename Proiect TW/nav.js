82c0f310cebe11459c1923553e450b8ccbf7a7fd
Antonio Falcescu <79379869+Rodioo@users.noreply.github.com>
2021-06-04 16:04:49 +0300

    Add files via upload

--- Proiect TW/nav.js
+++ Proiect TW/nav.js
@@ -26,6 +26,44 @@ function clearCookies(event)
     alert(event.target.id);
 }
     
+function validateForm()
+{
+    var x = document.forms["formular"]["inputText"].value;
+    if (x === "")
+    {
+        return false;
+    }
+}
+
+function searchWithButton()
+{
+    if(validateForm() === false)
+    {
+        alert("Please put some text to search for.");
+    }
+    else
+    {
+        var input = document.getElementById("searchBar");
+        var txt = document.getElementById("searchBar").value;
+        txt = "https://www.imdb.com/find?q=" + txt;
+        window.open(txt, '_blank');
+    }
     
+}
+
+function searchWithEnter(event)
+{
+    if(event.keyCode === 13)
+    {   
+        if(validateForm() !== false)
+        {
+            var input = document.getElementById("searchBar");
+            var txt = document.getElementById("searchBar").value;
+            txt = "https://www.imdb.com/find?q=" + txt;
+            window.open(txt, '_blank');
+            
+        } 
+    } 
+}   
 
 
